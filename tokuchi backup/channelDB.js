77f16ecf748606fffcb9633744b2df8b4888faef
ananksks123anan <83782074+ananksks123anan@users.noreply.github.com>
2021-05-11 07:40:09 +0300

    Delete tokuchi backup directory

--- tokuchi backup/channelDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const dark_channelDB = mongoose.Schema({
-  roleid: String,
-  rolename: String,
-  channels: Array
-});
-
-module.exports = mongoose.model("dark_channelDB", dark_channelDB);
