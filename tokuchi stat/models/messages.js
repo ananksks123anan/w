8a7d997341d3772fdfcc2fb319bfb0fc1a495cdc
ananksks123anan <83782074+ananksks123anan@users.noreply.github.com>
2021-05-11 07:39:59 +0300

    Delete tokuchi stat directory

--- tokuchi stat/models/messages.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const dark_messages = mongoose.Schema({
-  user: String,
-  channels: Map,
-  timeout: Number
-});
-
-module.exports = mongoose.model("dark_messages", dark_messages);
